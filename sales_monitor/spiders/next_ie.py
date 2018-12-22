08e1d1bfa6214df3ac133a474bb14c9faaeb867b
Xiang Li <xianglee@amazon.com>
2018-12-22 22:31:55 +0000

    Remove useless print code

--- sales_monitor/spiders/next_ie.py
+++ sales_monitor/spiders/next_ie.py
@@ -13,9 +13,6 @@ class NextIeSpider(BaseSpider):
 
         # The price can be a range. e.g. €18.50 - €25
         price_string = response.css('div.Price > div > span ::text').extract_first(default=0).replace('€', '')
-
-        print(price_string)
-
         price = price_string.split('-')[0].strip()
         item['price'] = Decimal(price)
 
